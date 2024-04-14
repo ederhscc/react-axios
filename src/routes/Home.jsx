1b8905a0626372e59a4f36f85569e4806354c912
Eder Henrique <ederhscc@gmail.com>
2024-04-14 20:22:50 -0300

    showing posts in the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,8 +11,9 @@ const Home = () => {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
-      const data = response.data
-      console.log(data);
+      const data = response.data;
+
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -22,7 +23,24 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;

df2a9e856358dae9e9a6ab3c923754002cae073e
Eder Henrique <ederhscc@gmail.com>
2024-04-21 21:35:03 -0300

    added base URL in the axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.css";
-import axios from "axios";
+import blogFetch from "../axios/config";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -8,9 +8,7 @@ const Home = () => {
 
   const getPosts = async () => {
     try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
       const data = response.data;
 
       setPosts(data);
